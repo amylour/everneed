fdac3acd71b067acf75a7ed0108bd38d24d3f429
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-08-16 20:22:27 +0000

    add: faq page

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('faq/', views.faq, name='faq'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def faq(request):
+    """ A view to return the FAQ page """
+
+    return render(request, 'home/faq.html')
