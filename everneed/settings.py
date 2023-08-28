ff6301dba9b7bd31114907337ef572344959c1da
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-08-28 21:13:58 +0000

    add: media context processor

--- everneed/settings.py
+++ everneed/settings.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
