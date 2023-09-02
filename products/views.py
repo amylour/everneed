76e8ef85cd45fade9160fe3362f04da52b3c2582
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-02 22:30:42 +0000

    add: category filtering/ fix active 'all' class bug

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 
@@ -10,8 +10,21 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    # Fix: 'UnboundLocalError' -> credit: https://tinyurl.com/yc847kb7
+    # & https://tinyurl.com/26a5ksrd
+    categories = [] # Default value to initialize as an empty list
 
     if request.GET:
+        if 'category' in request.GET:
+            selected_categories = request.GET.getlist('category')
+            if 'all' in selected_categories:
+                products = products  # Don't filter for 'all' categories
+            else:
+                products = products.filter(
+                    category__name__in=selected_categories)
+                categories = Category.objects.filter(
+                    name__in=selected_categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,6 +38,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
