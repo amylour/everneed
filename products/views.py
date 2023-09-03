d2b23803629bfc880d86311616debe6d701dacaa
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-03 20:49:30 +0000

    add: sorting JS & back to top link & styling

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
