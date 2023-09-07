507f7639b285ed916a70eabdcaa0be467686d79d
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-07 09:44:25 +0000

    add:bag quantity to bag icon

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -16,10 +29,13 @@ def bag_contents(request):
         free_delivery_delta = 0
 
     grand_total = delivery + total
+    # display bag quantity on bag icon
+    total_quantity = sum(bag.values())
 
     context = {
         'bag_items': bag_items,
         'total': total,
+        'total_quantity': total_quantity,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
--- bag/views.py
+++ bag/views.py
@@ -23,5 +23,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
