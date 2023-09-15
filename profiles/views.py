6d2dd179426919ce9335d1b08ee0c73f4d9defea
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-15 21:32:43 +0000

    add: basic profile account form & styling

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import UserProfile
+from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
+@login_required
 def profile(request):
-    """ A view to return the profiles page """
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
 
     return render(request, template, context)
