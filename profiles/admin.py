9c02adbc488b4b360399884b0b32ddf8b29a2147
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-17 19:15:14 +0000

    add: profile/account admin to django panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'full_name',
+        'default_phone_number',
+        'default_street_address1',
+        'default_street_address2',
+        'default_town_or_city',
+        'default_county',
+        'default_postcode',
+        'default_country',
+    )
+
+
+admin.site.register(UserProfile, AccountAdmin)
+
