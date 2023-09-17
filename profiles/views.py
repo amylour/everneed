d32bc3c250ce0bc9d4b8cdd186fa3b8023307949
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-17 21:45:56 +0000

    update: wishlist button and header icon for registered users

--- profiles/views.py
+++ profiles/views.py
@@ -17,7 +17,7 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
+            messages.success(request, 'Account updated successfully')
         else:
             messages.error(
                 request, 'Update failed. Please ensure the form is valid.')
