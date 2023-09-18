3dfe7ec249d7423727ca1690d6fc3a3768eedc23
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-18 14:21:33 +0000

    edit: remove full name field (possible reason for profile info save bug)

--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,6 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=50, null=False, blank=False, default="Customer")
     default_phone_number = models.CharField(
         max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(
