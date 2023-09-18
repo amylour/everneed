2cdce1c71dd484767c525b1c0c4e75e8eecb603a
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-18 17:33:11 +0000

    add: carbon totals to bag & checkout

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,6 +10,8 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+    total_carbon_fp = 0
+    total_carbon_saved = 0  
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
@@ -21,6 +23,11 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
+            # Calculate carbon footprint of order and carbon saved
+            total_carbon_fp += (item_data *
+                                product.carbon_fp) if product.carbon_fp else 0
+            total_carbon_saved += (item_data *
+                                   product.carbon_saved) if product.carbon_saved else 0
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
@@ -32,6 +39,10 @@ def bag_contents(request):
                     'product': product,
                     'size': size,
                 })
+                total_carbon_fp += (quantity *
+                                    product.carbon_fp) if product.carbon_fp else 0
+                total_carbon_saved += (quantity *
+                                       product.carbon_saved) if product.carbon_saved else 0
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -44,7 +55,6 @@ def bag_contents(request):
     # display bag item quantity on bag icon
     total_item_count = sum(item.get('quantity', 0) for item in bag_items)
 
-
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -54,6 +64,8 @@ def bag_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+        'total_carbon_fp': total_carbon_fp,
+        'total_carbon_saved': total_carbon_saved,
     }
 
     return context
