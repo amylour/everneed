df94c3737bae7c6cc1ab7e2425ab026035700ea0
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-22 18:00:57 +0000

    add: email order confirmation with DEFAULT_FROM_EMAIL

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -107,6 +107,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -151,6 +152,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
--- everneed/settings.py
+++ everneed/settings.py
@@ -197,6 +197,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'everneed@example.com'
 
 
 # Default primary key field type
