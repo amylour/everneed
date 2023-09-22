670adb2795a4dbf2382ec12b4688cb91a4ca1dc2
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-22 19:10:54 +0000

    add: login reuired decorators for security

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
 from .models import Product, Category
 from .forms import ProductForm
