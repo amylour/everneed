d7fe3087b7a80aabcf20e1767fbed13f53335c2a
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-22 19:06:28 +0000

    add: product form for admin dashboard

---
+++ products/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .widgets import CustomClearableFileInput
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    image = forms.ImageField(
+        label='Image', required=False, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
