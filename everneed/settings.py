4ae430f4a1e6bd73504c7d868c163785611d3cfa
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-24 14:15:51 +0000

    update S3 bucket name

--- everneed/settings.py
+++ everneed/settings.py
@@ -199,7 +199,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'everneed'
+    AWS_STORAGE_BUCKET_NAME = 'everneed-096aafe5d13c'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
