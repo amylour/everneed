26328dcce1aa05f054cdc5fa8dc2e03ccd2a98ca
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-09-27 14:57:20 +0000

    fix: settings for email

--- everneed/settings.py
+++ everneed/settings.py
@@ -241,7 +241,8 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')DEFAULT_FROM_EMAIL = 'everneld type
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
