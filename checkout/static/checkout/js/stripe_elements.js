bdb040bcafc6b761aa78d54de0341c7ce11fcbd5
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-10-09 10:41:26 +0000

    update: spell, format, dead code check

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -116,5 +116,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
