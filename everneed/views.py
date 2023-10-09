73036448673ffeae4f51cd8e6df1cc5da5a28537
AMY RICHARDSON <AMYLOUMC@GMAIL.COM>
2023-10-09 15:09:11 +0000

    fix: 404 views to remove error folder

--- everneed/views.py
+++ everneed/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    return render(request, "404.html", status=404)
 
 
 def error_view(request):
